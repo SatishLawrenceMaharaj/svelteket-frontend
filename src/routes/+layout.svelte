04e8ee62b5c0752190c474b097ea7609ffdd2d52
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-05-11 14:42:16 -0400

    Page preloaders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+
+
+	onMount(() => {
+		// preloader;
+		const preloader = document.querySelector('.preloader-wrapper');
+		setTimeout(() => {
+			preloader.classList.add('hidden');
+		}, 5500);
+	});
 </script>
+
+<div
+	class="preloader-wrapper h-screen flex justify-center items-center fixed top-0 left-0 z-50 w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
+>
+	<div id="loader" class="preloader" />
+	<h1 class="text-xl font-semibold text-white-600/100 dark:text-white ml-2 animate-bounce">
+		Page Loading...
+	</h1>
+</div>
+
+
+
 
 <slot />
