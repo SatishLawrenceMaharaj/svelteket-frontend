db0bd54011746731bc45386f4907edeefc9acc0b
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-05-12 01:50:41 -0400

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import eplogo from '$lib/images/eplogo.png';
 	import { onMount } from 'svelte';
 	import {
 		Navbar,
@@ -20,8 +21,28 @@
 		Footer,
 		FooterLink,
 		FooterLinkGroup,
-		FooterCopyright
+		FooterCopyright,
+		Textarea,
+		ToolbarButton
 	} from 'flowbite-svelte';
+
+	let active = '';
+	onMount(() => {
+		let url = window.location.href;
+		if (url.includes('my-profile')) {
+			active = 'Profile';
+		} else if (url.includes('settings')) {
+			active = 'Settings';
+		} else if (url.includes('report')) {
+			active = 'Report';
+		} else if (url.includes('contact')) {
+			active = 'Contact';
+		} else if (url.includes('catalog')) {
+			active = 'Catalog';
+		} else {
+			active = 'Home';
+		}
+	});
 
 	onMount(() => {
 		// preloader;
@@ -41,13 +62,176 @@
 	</h1>
 </div>
 
+<div class="grid gap-2 grid-cols-2 sticky top-0">
+	<div class="column border-b border-black-500 w-screen z-40">
+		<Navbar let:hidden let:toggle>
+			<NavBrand href="/">
+				<img src={eplogo} class="mr-3 h-6 sm:h-9" alt="EduPro Logo" />
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+					EduPro
+				</span>
+			</NavBrand>
+			<NavHamburger on:click={toggle} />
+			<NavUl {hidden}>
+				<NavLi active={active === 'Home'} href="/" on:click={() => (active = 'Home')}>Home</NavLi>
+				<NavLi active={active === 'About'} href="/" on:click={() => (active = 'About')}>About</NavLi
+				>
+				<NavLi active={active === 'Users'} href="/" on:click={() => (active = 'Users')}>Users</NavLi
+				>
+			</NavUl>
+		</Navbar>
+	</div>
+	<div class="column z-10">
+		<aside
+			id="logo-sidebar"
+			class="fixed top-0 left-0 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 z-10"
+			aria-label="Sidebar"
+		>
+			<div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+				<NavBrand href="/" class="space-y-2">
+					<span
+						class="self-center whitespace-nowrap text-xl font-semibold dark:text-white mt-1 mb-2 ml-2"
+					>
+						Chats
+					</span>
+				</NavBrand>
+				<hr class="h-px bg-gray-200 border-0 dark:bg-gray-700 mb-5" />
+
+				<ul class="space-y-2 font-medium">
+					<li class="border-none border-2 border-sky-500 hover:border-solid">
+						<a
+							href="#"
+							class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+						>
+							<div class="flex items-center space-x-4">
+								<img
+									class="w-10 h-10 rounded-full"
+									src="https://banner2.cleanpng.com/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg"
+									alt=""
+								/>
+								<div class="font-medium dark:text-white">
+									<div>Satish Maharaj</div>
+								</div>
+							</div>
+						</a>
+						<hr class="h-px bg-gray-200 border-0 dark:bg-gray-700" />
+					</li>
+
+					<li class="border-none border-2 border-sky-500 hover:border-solid">
+						<a
+							href="#"
+							class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+						>
+							<div class="flex items-center space-x-4">
+								<img
+									class="w-10 h-10 rounded-full"
+									src="https://banner2.cleanpng.com/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg"
+									alt=""
+								/>
+								<div class="font-medium dark:text-white">
+									<div>John Doe</div>
+								</div>
+							</div>
+						</a>
+						<hr class="h-px bg-gray-200 border-0 dark:bg-gray-700" />
+					</li>
+				</ul>
+			</div>
+		</aside>
+	</div>
+</div>
+
+<div class="p-4 sm:ml-64" id="body">
+	<div
+		class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700"
+		style="height:80vh"
+	>
+		<img src={eplogo} alt="EduPro Logo" class="w-4/12 mx-auto my-auto" />
+	</div>
+
+	<div class="relative h-16 w-full mt-4">
+		<div class="absolute inset-x-0 bottom-0 h-16">
+			<form>
+				<label for="chat" class="sr-only">Your message</label>
+				<Alert color="dark" class="px-3 py-2">
+					<svelte:fragment slot="icon">
+						<ToolbarButton color="dark" class="text-gray-500 dark:text-gray-400">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+								><path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+								/></svg
+							>
+							<span class="sr-only">Upload image</span>
+						</ToolbarButton>
+						<ToolbarButton color="dark" class="text-gray-500 dark:text-gray-400">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+								><path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+								/></svg
+							>
+							<span class="sr-only">Add emoji</span>
+						</ToolbarButton>
+						<Textarea id="chat" class="mx-4" rows="1" placeholder="Your message..." />
+						<ToolbarButton
+							type="submit"
+							color="blue"
+							class="rounded-full text-blue-600 dark:text-blue-500"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+								><path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+								/></svg
+							>
+							<span class="sr-only">Send message</span>
+						</ToolbarButton>
+					</svelte:fragment>
+				</Alert>
+			</form>
+		</div>
+	</div>
+</div>
+
 <slot />
 
 <svelte:head>
-	<title>Home</title>
+	<title>EduPro</title>
 </svelte:head>
 
 <style>
+	*{
+		overflow-y: hidden !important;
+		overflow-x: hidden !important;
+	}
+
+	#body{
+		overflow-y: hidden !important;
+		overflow-x: hidden !important;
+	}
+
 	/*preloader css*/
 	.preloader {
 		width: 100px;
